9e497c78512f09c65d094cc077a69af48cb3e9f3
Xushnudbek Raimov <12xushnudbek34@gmail.com>
2024-11-28 01:06:38 +0500

    fix: update not found

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,18 +1,22 @@
 import Link from "next/link";
-import { FC } from "react";
+import { FC, Suspense } from "react";
 
 interface NotFoundProps {}
 
 const NotFound: FC<NotFoundProps> = ({}) => {
   return (
-    <div className='w-screen h-screen flex justify-center items-center'>
-      <Link
-        href='/'
-        className='border-black/30 dark:border-white/30 border px-5 py-2 rounded-lg'
-      >
-        Go to home
-      </Link>
-    </div>
+    <Suspense fallback={<div>Loading...</div>}>
+      <div className='w-screen h-screen flex justify-center flex-col gap-3 items-center'>
+        <h1>404 - Page Not Found</h1>
+        <p>We couldn’t find the page you were looking for.</p>
+        <Link
+          href='/'
+          className='border-black/30 dark:border-white/30 border px-5 py-2 rounded-lg'
+        >
+          Go to home
+        </Link>
+      </div>
+    </Suspense>
   );
 };
 
